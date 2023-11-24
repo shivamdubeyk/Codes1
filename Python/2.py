c0a3c1fe1d6346f2f4874c4a842979188729d455
shivamdubeyk <118098939+shivamdubeyk@users.noreply.github.com>
2023-11-25 00:46:21 +0530

    Excel college and python

---
+++ Python/2.py
@@ -0,0 +1,5 @@
+#Addition of two numbers
+a=20 
+b=20
+c=a+b
+print(c)
---
+++ Python/3.py
@@ -0,0 +1,10 @@
+"""name="'didi wefef'"
+print(name)
+a=20 
+b=30
+c=a+b
+print("value of %d is")"""
+a=input("ENTER A")
+b=input("ENTER B")
+c=a+b
+print("value of c is ",c)
